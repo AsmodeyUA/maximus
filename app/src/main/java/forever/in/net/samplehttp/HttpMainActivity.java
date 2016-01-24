b73d99e48c3eadf89beb6e8010386b5da279ae63
AsmodeyZ <AnatoliySerheyev@gmail.com>
2016-01-24 17:27:28 +0200

    initial commit

---
+++ app/src/main/java/forever/in/net/samplehttp/HttpMainActivity.java
@@ -0,0 +1,13 @@
+package forever.in.net.samplehttp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HttpMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http_main);
+    }
+}
